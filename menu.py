a49204d1f68b3c4ad251be1d1e9fa19980d6221a
VM170798 <viktormaczurek@email.cz>
2025-11-25 21:59:47 +0100

    Zmena nazvu

--- menu.py
+++ menu.py
@@ -13,31 +13,31 @@ class Menu:
     def get_choice():
         while True:
             try:
-                choice = int(input("\nEnter your choice: "))
+                choice = int(input("\nZadejte cislo volby: "))
                 if choice in [1, 2, 3, 4, 5, 6]:
                     return choice
                 else:
-                    print("\033[93mOption does not exist. Please try again.\033[0m")
+                    print("\033[93mVolba je neplatna. Zkuste to prosim znovu.\033[0m")
             except ValueError:
-                print("\033[93mInvalid input. Please enter a number from the options above.\033[0m")
+                print("\033[93mNeplatny vstup. Prosim vlozte cislo z horni nabidky.\033[0m")
 
     #metoda pro zobrazeni menu a zpracovani volby uzivatele
     def show(self):
-        print("\nWelcome to the enhanced Task Manager with MySql DB!")
+        print("\nVitejte ve vylepsenem Task Manager s MySql DB!")
 
         while True:
-            print("Please choose an option:")
-            print("1. Create Task")
-            print("2. Update Task")
-            print("3. List all tasks")
-            print("4. List all tasks (deleted included)")
-            print("5. Remove task")
-            print("6. Exit program")
+            print("Prosim zvolte volbu:")
+            print("1. Vytvorit Zadani")
+            print("2. Aktualizovat Zadani")
+            print("3. Zobraz vsechna zadani")
+            print("4. Zobraz vsechna zadani (vcetne vymazanych)")
+            print("5. Odstranit zadani")
+            print("6. Ukoncil program")
             user_choice = self.get_choice()
 
             #ukoncujem program, pokud uzivatel zvoli 6, ale da se to resit i jinak
             if user_choice == 6:
-                print("\nThank you for using the Task Manager. Goodbye!")
+                print("\nDekuji ze pouzivate Task Manager. Nashledanou!")
                 exit()
             else:
                 #tady to pro zjednoduseni necham zpracovat tou servisou, ktera si sama kontroluje vstup a jestli vstup je validni
